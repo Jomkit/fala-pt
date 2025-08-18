cca963ef283821b98d8a9e4af44604b4df69f861
Jomkit <j.jujaroen96@gmail.com>
2025-08-17 21:27:47 -0400

    setup frontend next.js

---
+++ frontend/src/app/vocabulary/layout.tsx
@@ -0,0 +1,7 @@
+export default function VocabularyLayout({children}: {children: React.ReactNode}) {
+    return (
+        <section>
+            {children}
+        </section>
+    );
+}
---
+++ frontend/src/app/vocabulary/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="flex w-full justify-center">
+            <h1 className="text-5xl mt-5">Vocabulary</h1>
+        </div>
+    );
+}
